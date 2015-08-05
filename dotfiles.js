8e0fa7eb3d84b4be1f9305ffb738d422c73ec71c
Filipe Linhares <lip.linhares@gmail.com>
2015-08-05 10:55:50 -0300

    Adds test instalation

--- dotfiles.js
+++ dotfiles.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 
-var util  = require('./src/util');
+var util   = require('./src/util');
 var aptGet = require('./softwares/apt-get');
 var clones = require('./softwares/git-clone');
+var wget   = require('./softwares/wget');
 
-var examples = [
-    'opera'
-];
-
-util.question('apt-get', aptGet.choices, function (answers) {
+util.question('apt-get', aptGet.choices).then(function (answers) {
   util.addPPA(answers['apt-get'], aptGet.ppa);
   util.addKey(answers['apt-get'], aptGet.key);
-}, 'What do you want to install?');
+  util.aptGetInstall(answers['apt-get'], aptGet.softwares);
+
+  // # Returning new question
+  return util.question('wget', wget.choices);
+
+}).then(function (answers) {
+    util.installWithWget(answers['wget'], wget.url);
+
+  return util.question('git-clone', clones.choices);
+}).then(function (answers) {
+  util.gitClone(answers['git-clone'], clones.repos);
+});
+
 
